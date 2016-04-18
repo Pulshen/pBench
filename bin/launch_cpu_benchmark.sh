663fd2fb2422de56ac26e513fcf764ea8553c383
sudokamikaze <pulshencode@outlook.com>
2016-04-18 23:05:40 +0300

    Writed menu to select autodetection or manual write numbers of cores

--- bin/launch_cpu_benchmark.sh
+++ bin/launch_cpu_benchmark.sh
@@ -2,8 +2,26 @@
 echo Welcome to benchmark!
 cd ../PulshenWRT*
 figlet CPU bench
-START=$(date +%s)
-make ${MAKEFLAGS="-j$(nproc)"} V=-1
+echo =====================================
+echo 1 Automatic detection of cpu cores
+echo "2 Manual write(numbers of cores)"
+echo =====================================
+echo -n "Choose an action: "
+read item
+case "$item" in
+  1) echo Detected $(nproc) cores, starting benchmark testing
+  START=$(date +%s)
+  make ${MAKEFLAGS="-j$(nproc)"} V=-1
+  ;;
+  2) echo -n "How much u have cpu cores: "
+  read cores
+  START=$(date +%s)
+  echo "Compilation stated on $cores cores"
+  make ${MAKEFLAGS="-j$cores"} V=-1
+  ;;
+  *) echo "Unknown symbol"
+  ;;
+esac
 END=$(date +%s)
 DIFF=$(( ( $END - $START)/5 *2 ))
 cd ..
