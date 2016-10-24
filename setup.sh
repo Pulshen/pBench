38d44fb42db62acbd73c00280e9a3faf174b252f
Sudokamikaze <pulshencode@outlook.com>
2016-10-24 16:21:14 +0300

    Some enchantments

--- setup.sh
+++ setup.sh
@@ -1,30 +1,32 @@
 #!/bin/bash
-echo "Installing libs for compiling"
-echo ======================
-echo "1. ArchLinux(PACMAN)"
-echo "2. Debian based(APT)"
-echo "3. Skip this step"
-echo =======================
-echo -n "Choose an action: "
-read item
-case "$item" in
-    1) echo "Preparing to install utils"
-       sudo pacman -S --needed dmidecode subversion asciidoc bash bc binutils bzip2 fastjar flex git gcc util-linux gawk intltool zlib make cdrkit ncurses openssl patch perl-extutils-makemaker rsync sdcc unzip wget gettext libxslt boost libusb bin86 sharutils b43-fwcutter findutils
-        ;;
-    2) echo "Starting APT..."
-       sudo apt-get install dmidecode subversion git g++ libncurses5-dev zlib1g-dev gawk figlet make libssl-dev
-        ;;
-    3) echo "Skiping lib install procedure"
-        ;;
-    *) echo "Error, unknown symbol"
-            ;;
+
+function wrt {
+  mkdir PulshenWRT_build
+  git clone git@github.com:lede-project/source.git PulshenWRT_build
+  git clone git@github.com:Sudokamikaze/PulshenWRT.git
+  cp PulshenWRT/configs_default/config_lede_trunk PulshenWRT_build/.config
+  rm -rf PulshenWRT
+  cd PulshenWRT_build
+  ./scripts/feeds update -a && ./scripts/feeds install -a
+  make menuconfig
+}
+
+echo -n "Install libs for building? Y/n: "
+read libs
+case "$libs" in
+  y|Y) echo "Installing..."
+  eval $(grep ID= /etc/os-release)
+  if [ $ID == arch ]; then sudo pacman -S --needed dmidecode subversion asciidoc bash bc binutils bzip2 fastjar flex git gcc-multilib util-linux gawk intltool zlib make cdrkit ncurses openssl patch perl-extutils-makemaker rsync sdcc unzip wget gettext libxslt boost libusb bin86 sharutils b43-fwcutter findutils
+elif [ $ID == ubuntu ]; then sudo apt-get install dmidecode subversion git g++ libncurses5-dev zlib1g-dev gawk libssl-dev
+elif [ $ID == debian ]; then sudo apt-get install dmidecode subversion git g++ libncurses5-dev zlib1g-dev gawk libssl-dev
+  else
+   echo Unsuported distro, u may install this manualy.
+  fi
+  echo Done!
+  ;;
+  n|N)
+  ;;
 esac
-mkdir PulshenWRT_build
-git clone git@github.com:lede-project/source.git PulshenWRT_build
-git clone git@github.com:Sudokamikaze/PulshenWRT.git
-cp PulshenWRT/configs_default/config_lede_trunk PulshenWRT_build/.config
-rm -rf PulshenWRT
-cd PulshenWRT_build
-./scripts/feeds update -a && ./scripts/feeds install -a
-make menuconfig
+
+wrt
 echo Done!
